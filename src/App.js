a3d8b03d3a60c3e0a87f22f5bfdaa42d273b229e
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-08-03 17:10:37 -0700

    added a flower

--- src/App.js
+++ src/App.js
@@ -5,17 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>hello</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <strong>🌸</strong>
       </header>
     </div>
   );

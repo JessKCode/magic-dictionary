c10f734e5bb2cbf657535a90e36b2a86c57eaea4
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-08-12 17:46:18 -0700

    added a Results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary (){
   let [keyword, setKeyword] = useState ("");
+  let [result, setResult] = useState({});
 
   function handleKeywordChange (event){
     setKeyword(event.target.value);
@@ -11,25 +13,25 @@ export default function Dictionary (){
   }
 
   function handleResponse(response){
-    console.log(response.data);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResult(response.data[0])
 
   }
 
   function search(event){
     event.preventDefault(); 
-    alert(`Searching for ${keyword} ...`);
-
+    
+    // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
-
   }
   
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
       <input type="search" autoFocus={true} onChange={handleKeywordChange} />
-
       </form> 
+      <Results results={result} />
     </div>
   )
 }

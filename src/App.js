3a4d493afcdd5f035d967ecf2dc2e939c19cc136
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-08-22 16:56:35 -0700

    added github and netlify link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App() {
         <Dictionary defaultKeyword="Cat"/>
       </main>
       <footer className="App-footer">
-        Coded by Jessica Carter
+        This project was coded by Jessica K Carter and is open-sourced on {""}
+         <a href="https://github.com/JessKCode/weather-watcher-"
+      target="_blank"
+      rel="noreferrer">GitHub </a>
+       and hosted on <a href="https://www.netlify.com/?utm_source=google&utm_medium=paid_search&utm_campaign=12755510784&adgroup=118788138897&utm_term=netlify&utm_content=aud-1224736163202:kwd-371509120223&creative=514583565825&device=c&matchtype=e&location=9033614&gclid=CjwKCAjw64eJBhAGEiwABr9o2E8Trsu9GKJ0iu4PKzjwiSOE8FKgPZ-9smPAE4TOiW35_NNT5eAwVxoCTlAQAvD_BwE" target="_blank"
+      rel="noreferrer">Netlify </a> 
       </footer>
     </div>
   );
